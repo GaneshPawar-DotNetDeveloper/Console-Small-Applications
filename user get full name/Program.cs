949f25e5f94983538c2cc3d9f0f871e7887e5dfe
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-04-16 22:27:20 +0530

    Add files via upload

---
+++ user get full name/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace user_get_full_name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your First Name :");
+            String FirstName=Console.ReadLine();
+            Console.WriteLine("Please Enter Your Last Name:");
+            String LastName=Console.ReadLine();
+            string FullName = $"FullName is : {FirstName} {LastName}";
+            Console.WriteLine(FullName);
+            Console.ReadLine();
+        }
+    }
+}
